879e74c8f533d975a47e68bd738a859c50e73dcb
xraybat <xraybat100@gmail.com>
2020-02-20 13:42:45 +1100

    disambiguating my and cask castor contexts

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,9 @@
 import fsm.machine.pingpong._
-import fsm.machine.ContextPrefs._
+import fsm.machine._
  
 object Main {
   def main(args: Array[String]): Unit = {
-    val pp = new PingPongPlayer(java.time.Duration.ofMillis(20))
+    // can specify 'ac' context specifically. helpful since cask uses castor too.
+    val pp = new PingPongPlayer(java.time.Duration.ofMillis(20))(ContextPrefs.ac)
   }
 }
