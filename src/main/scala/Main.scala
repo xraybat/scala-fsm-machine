c861e7e09101e8f41a9649875913094ca21c80fa
xraybat <xraybat100@gmail.com>
2020-02-20 14:30:11 +1100

    minor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import fsm.machine._
  
 object Main {
   def main(args: Array[String]): Unit = {
-    // can specify 'ac' context specifically. helpful since cask uses castor too.
+    // can specify 'ac' context explicitly. helpful since cask uses castor too.
     val pp = new PingPongPlayer(java.time.Duration.ofMillis(20))(ContextPrefs.ac)
   }
 }
